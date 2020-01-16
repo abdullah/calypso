03a573363c9c56af9a83f0643cfd078e8b2a4be5
Abdullah mara <mara.abdullah00@gmail.com>
2020-01-16 14:45:20 +0300

    update path

--- defaultConfig.js
+++ defaultConfig.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: '.',
   languages: {
-    base: 'i18n/en.json',
+    base: 'src/i18n/en.json',
     list: [
-      'i18n/en.json',
-      'i18n/tr.json',
+      'src/i18n/en.json',
+      'src/i18n/tr.json',
     ]
   },
   // extra: {
-  //   base: 'i18n/en_extra.json',
+  //   base: 'src/i18n/en_extra.json',
   //   list: [
-  //     'i18n/en_extra.json',
-  //     'i18n/tr_extra.json',
-  //     'i18n/fr_extra.json',
+  //     'src/i18n/en_extra.json',
+  //     'src/i18n/tr_extra.json',
+  //     'src/i18n/fr_extra.json',
   //   ]
   // },
 };
